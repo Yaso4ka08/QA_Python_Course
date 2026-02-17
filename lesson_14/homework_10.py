06e5077d39e1de8da0a5519165e234b9732b4498
Yaso4ka08 <diyalu27@gmail.com>
2026-02-16 19:06:50 -0500

    finally finished with logging tasks along with my prefernsiation of parametrization

---
+++ lesson_14/homework_10.py
@@ -0,0 +1,52 @@
+"""
+Ваша команда та ви розробляєте систему входу для веб-додатка,
+і вам потрібно реалізувати тести на функцію для логування подій в системі входу.
+Дано функцію, напишіть набір тестів для неї.
+"""
+
+import logging
+
+import pytest
+
+
+def log_event(username: str, status: str):
+    """
+    Логує подію входу в систему.
+
+    username: Ім'я користувача, яке входить в систему.
+
+    status: Статус події входу:
+
+    * success - успішний, логується на рівні інфо
+    * expired - пароль застаріває і його слід замінити, логується на рівні warning
+    * failed  - пароль невірний, логується на рівні error
+    """
+    log_message = f"Login event - Username: {username}, Status: {status}"
+
+    # Створення та налаштування логера
+    logging.basicConfig(
+        filename='login_system.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s'
+        )
+    logger = logging.getLogger("log_event")
+
+    # Логування події
+    if status == "success":
+        logger.info(log_message)
+    elif status == "expired":
+        logger.warning(log_message)
+    else:
+        logger.error(log_message)
+
+@pytest.mark.parametrize("username, status", [
+    "admin, expired",
+    "admin, success",
+    "user_1, expired",
+    "user_2, success"
+    ]
+)
+def test_log_event_success(username,status):
+    with open (login_system.log) as log:
+        content = log.read()
+        assert f"Login event - Username: {username}, Status: {status}"
